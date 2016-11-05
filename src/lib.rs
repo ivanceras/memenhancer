725706fa1da5292a0e707767ca673c9ea658576d
Jovansonlee Cesar <ivanceras@gmail.com>
2016-11-05 23:16:04 +0800

    Added body, rest of the unparsed text, regrouped the rest of the text

--- src/lib.rs
+++ src/lib.rs
@@ -30,12 +30,33 @@ impl Default for Settings {
     }
 }
 
-enum Anchor{
+pub enum Anchor{
     Start,
     Middle,
     End
 }
 
+
+struct Body{
+    memes: Vec<Meme>,
+    rest_str: Vec<(usize, String)>
+}
+
+impl Body {
+
+    
+    fn get_svg_elements(&self, settings: &Settings) -> Vec<Box<Node>>{
+        let mut svg:Vec<Box<Node>> = vec![];
+        for &(startx, ref text)  in &self.rest_str{
+            let svg_text = to_svg_text(text, startx, 0, settings, Anchor::Start);    
+            svg.push(Box::new(svg_text));
+        }
+        for meme in &self.memes{
+            svg.extend(meme.get_svg_elements(settings));
+        }
+        svg
+    }
+}
 
 /// The whole meme body
 /// 
@@ -58,7 +79,7 @@ impl Meme{
         let mut elements = vec![];
         elements.extend(self.head.get_svg_elements(settings));
         let right_text = to_svg_text(&self.right_side, self.head.endx, 0, settings, Anchor::Start);
-        let left_text = to_svg_text(&self.left_side, self.head.startx - 1, 0, settings, Anchor::End);
+        let left_text = to_svg_text(&self.left_side, self.head.startx, 0, settings, Anchor::End);
         elements.push(Box::new(left_text));
         elements.push(Box::new(right_text));
         elements
@@ -67,11 +88,16 @@ impl Meme{
     
 }
 
-
 fn to_svg_text(s: &str, x: usize, y: usize, settings: &Settings, anchor: Anchor) -> SvgText {
+    let px = x as f32 * settings.text_width;
+    let py = y as f32 * settings.text_height;
+    to_svg_text_pixel(s, px, py, settings, anchor)
+}
+
+fn to_svg_text_pixel(s: &str, x: f32, y: f32, settings: &Settings, anchor: Anchor) -> SvgText {
     let (offsetx, offsety) = settings.offset();
-    let sx = x as f32 * settings.text_width + settings.text_width / 4.0 + offsetx;
-    let sy = y as f32 * settings.text_height + settings.text_height * 3.0 / 4.0 + offsety;
+    let sx = x + offsetx;
+    let sy = y + settings.text_height * 3.0 / 4.0 + offsety;
     let mut svg_text = SvgText::new()
         .set("x", sx)
         .set("y", sy);
@@ -119,10 +145,6 @@ impl Head{
         self.endx - self.startx     
     }
 
-    fn face_width(&self) -> usize{
-        self.face.width()
-    }
-    
     fn get_svg_elements(&self, settings:&Settings) -> Vec<Box<Node>> {
         let mut elements: Vec<Box<Node>> = vec![];
         elements.push(Box::new(self.get_circle(settings)));
@@ -131,34 +153,40 @@ impl Head{
     }
 
     fn get_face_text(&self, settings: &Settings) -> SvgText{
-        to_svg_text(&self.face, self.startx + 1, 0, settings, Anchor::Start)
+        let c = self.calc_circle(settings);
+        to_svg_text_pixel(&self.face, c.cx, 0.0, settings, Anchor::Middle)
     }
 
-    fn get_circle(&self, settings: &Settings)-> SvgCircle{
+    fn calc_circle(&self, settings: &Settings) -> Circle {
         let (offsetx, offsety) = settings.offset();
         let text_width = settings.text_width;
         let text_height = settings.text_height;
-        let xloc = self.startx as f32 * text_width;
         let radius = self.distance() as f32 / 2.0;
-        let startx = self.startx as f32;
-        let endx = self.endx as f32;
-        let center = startx + radius;
-        let cx = center * text_width; 
+        let center = self. startx as f32 + radius;
+        let cx = center * text_width + offsetx; 
         let cy = text_height / 2.0 + offsety;
         let cr = radius * text_width;
+        Circle{
+            cx: cx,
+            cy: cy,
+            r: cr
+        }
+    }
 
+    fn get_circle(&self, settings: &Settings)-> SvgCircle{
+        let c = self.calc_circle(settings);
         SvgCircle::new()
-            .set("cx",cx)
-            .set("cy", cy)
-            .set("r", cr)
+            .set("cx",c.cx)
+            .set("cy", c.cy)
+            .set("r", c.r)
     }
 
 }
 
 #[derive(Debug)]
 pub struct Circle{
-    x: f32,
-    y: f32,
+    cx: f32,
+    cy: f32,
     r: f32,
 }
 
@@ -219,50 +247,13 @@ pub fn get_svg_elements(s: &str, text_width: f32, text_height: f32) -> Vec<Box<N
                     text_width: text_width,
                     text_height: text_height
                    };
-    let memes = parse_memes(s);
-    let mut svg = vec![];
-    for meme in memes{
-        let mel = meme.get_svg_elements(&settings);
-        svg.extend(mel);
-    }
-    svg
-}
-
-fn circle_to_svg(circle: &Circle) -> SvgCircle {
-    SvgCircle::new() 
-        .set("cx", circle.x)
-        .set("cy", circle.y)
-        .set("r", circle.r)
-}
-
-pub fn get_circles(s: &str, x: usize, y:usize, 
-        text_width: f32, text_height: f32) -> Vec<Circle>{
-    let mut circles = vec![];
-    let xloc = x as f32 * text_width;
-    let yloc = y as f32 * text_height;
-    let memes = parse_memes(s);
-    for m in memes{
-        let head = m.head;
-        let radius = head.face_width() as f32 / 2.0 + 0.5;
-        let startx = head.startx as f32;
-        let endx = head.endx as f32;
-        let left_adjustment = 0.5 + startx * 0.1;
-        let right_adjustment = 0.5 + endx * 0.1;
-        let center = 0.5 + startx + radius;
-        let comp_center = center * text_width + xloc;
-        let circle = Circle{
-            x: comp_center,
-            y: yloc + text_height / 2.0,
-            r: radius * text_width
-        };
-        circles.push(circle);
-    }
-    circles
+    let body = parse_memes(s);
+    body.get_svg_elements(&settings)
 }
 
 
-
-fn parse_memes(s: &str) -> Vec<Meme> {
+/// TODO: include parsing the rest of the unused text
+fn parse_memes(s: &str) -> Body{
     let mut memes = vec![];
     let mut paren_opened = false;
     let mut meme_face = String::new();
@@ -272,24 +263,31 @@ fn parse_memes(s: &str) -> Vec<Meme> {
     let mut startx = 0;
     let mut start_position = 0;
     let mut meme_start = 0;
-    let mut meme_end = 0;
     let mut meme_body = String::new();
     let mut meme_left_side = String::new();
     let mut meme_right_side = String::new();
     let mut meme_head = None;
     let total_chars = s.chars().count();
+    let mut rest_text:Vec<(usize, String)> = vec![];
     for ch in s.chars(){
         let last_char = index == total_chars - 1;
-        if meme_head.is_none() && ch == ' '{
+        if meme_head.is_none() && (ch == ' ' || last_char){
             meme_start = index + 1;
+            if !paren_opened{
+                let mut rest_word = meme_body.clone();
+                let rest_start = total_width - rest_word.width();
+                if last_char{
+                   rest_word.push(ch);
+                }
+                rest_text.push((rest_start, rest_word));
+            }
             meme_body.clear();
         }
         if meme_head.is_some() && (ch == ' ' || last_char){
-            meme_end = index;     
             let meme = Meme{
                start_position: meme_start,  
                head: meme_head.clone().unwrap(),
-               end_position: meme_end,
+               end_position: index,
                left_side: meme_left_side.clone(), 
                right_side: meme_right_side.clone(),
             };
@@ -332,7 +330,35 @@ fn parse_memes(s: &str) -> Vec<Meme> {
         }
         index += 1;
     } 
-    memes
+    //println!("rest_text: {:#?}", rest_text);
+    Body{
+        memes: memes,
+        rest_str: regroup_rest_text(&rest_text)
+    }
+}
+
+fn regroup_rest_text(rest_text: &Vec<(usize, String)>)->Vec<(usize, String)>{
+    let mut new_group = vec![];
+    //println!("regrouping text..");
+    for &(start,ref rest) in rest_text{
+        if new_group.is_empty(){
+            new_group.push((start, rest.clone()));
+        }else{
+            if let Some((lastx, last_rest)) = new_group.pop(){
+               if lastx + last_rest.width() == start{
+                    let mut merged = String::new();
+                    merged.push_str(&last_rest);
+                    merged.push_str(rest);
+                    new_group.push((lastx, merged));
+               }else{
+                   new_group.push((lastx, last_rest));
+                   new_group.push((start, rest.clone()));
+               } 
+            }
+        }
+    }
+    //println!("new_group: {:#?}", new_group);
+    new_group
 }
 
 #[test]
@@ -406,8 +432,8 @@ fn test_body2(){
 }
 
 #[test]
-fn test_position(){
-    let meme = "ヘ( ^o^)ノ ＼(^_^ )Gimme Five";
+fn test_rest_of_text(){
+    let meme = r#"The rest of   凸(•̀_•́)凸❤️ ( ͡° ͜ʖ ͡°) \(°□°)/层∀  the text is here"#;
     println!("{}", meme);
     let bodies = parse_memes(meme);
     for b in &bodies{
